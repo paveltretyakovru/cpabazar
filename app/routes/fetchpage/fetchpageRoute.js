ecf5098dd0ae1393e4259989a7eadb061d4a7278
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-08-16 17:00:00 +0300

    Move to promise fetch campaigns page

--- app/routes/fetchpage/fetchpageRoute.js
+++ app/routes/fetchpage/fetchpageRoute.js
@@ -2,41 +2,59 @@
 var express = require('express');
 var router = express.Router();
 var Campaign = require('../../Campaign');
+var Promise = require('promise');
+
+// Создаем обещание загрузки кампаний с зависимостями
+let Campaigns = new Promise(function(resolve, reject) {
+  Campaign.fetchAll().then(campaigns => {
+    campaigns.load(['pap', 'banners', 'commissiontypes'])
+      .then(campaigns => { return resolve(campaigns);})
+      .catch(err => { return reject(err) });
+  });
+});
+
+function getCountrycodes(commissiontypes) {
+  let result = [];
+
+  if(commissiontypes) {
+    commissiontypes.map(commission => {
+      let countrycodes = commission.countrycodes
+      if(countrycodes !== '' && countrycodes !== null) {
+        result.push(commission.countrycodes.split(','));
+      }
+    });
+  }
+
+  return result;
+}
 
 /* GET home page. */
 router.get('/', (req, res) => {
 
-  Campaign.fetchAll().then(campaigns => {
-    setTimeout(function(){
+  setTimeout(function(){
+    Promise.all([Campaigns]).then(values => {
+      // Сохраняем полученные обещания
+      let campaigns = values[0].toArray();
 
-      // Загружаем зависимости
-      campaigns.load(['pap', 'banners', 'commissiontypes']).then(campaigns => {
-        campaigns = campaigns.toArray();
+      let result = campaigns.map(element => {
+        let campaign = element.toJSON();
+        let geo = getCountrycodes(campaign.commissiontypes);
 
-        let result = campaigns.map(campaign => {
-          campaign = campaign.toJSON();
-
-          // let geo = campaign.commissiontypes.map(commission => {
-          //   if(commission.countrycodes !== '') {
-          //     return commission.countrycodes.split(',');
-          //   }
-          // });
-
-          // Формируем резульатат запроса
-          return {
-            id: campaign.id,
-            // geo: geo,
-            pap: campaign.pap,
-            price: campaign.price,
-            banners: campaign.banners,
-            commissiontypes: campaign.commissiontypes,
-          }
-        });
-
-        res.json({campaigns: result});
+        // Формируем резульатат запроса
+        return {
+          id: campaign.id,
+          geo: geo,
+          pap: campaign.pap,
+          price: campaign.price,
+          banners: campaign.banners,
+          commissiontypes: campaign.commissiontypes,
+        }
       });
-    }, 1000)
-  });
+
+      res.json({campaigns: result});
+    });
+  }, 1000);
+
 });
 
 module.exports = router;
