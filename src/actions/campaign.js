278230ac5681f1968ad5062c60a1cb1a8c3cc056
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-08-17 21:24:03 +0300

    Создание обработчика отправки данных формы пользователя

--- src/actions/campaign.js
+++ src/actions/campaign.js
@@ -2,6 +2,7 @@ import {
 
   SWITCH_DIALOG,
   UPDATE_PROFFER_FORM_DATA,
+  SEND_PROFFER_DATA,
 
 } from '../constants/campaign';
 
@@ -14,6 +15,12 @@ export function switchDialog() {
 export function updateProfferFormData(key, value) {
   return {
     type: UPDATE_PROFFER_FORM_DATA,
-    payload: { key: key, value: value},
+    payload: { key: key, value: value },
+  }
+}
+
+export function sendProfferData() {
+  return {
+    type: SEND_PROFFER_DATA,
   }
 }
